5abff954a85801e3706992164cd45d3288cd2f83
Vignesh Ashokan <vignesh129@gmail.com>
2017-02-09 08:51:54 -0600

    added responsive images

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,10 +4,11 @@ var gulp = require("gulp"),
     cssvars = require("postcss-simple-vars"),
     nested = require("postcss-nested"),
     cssImport = require("postcss-import");
+    mixins = require("postcss-mixins");
 
 gulp.task("styles", function(){
   return gulp.src("./app/assets/styles/styles.css")
-  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+  .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
   .on("error", function(err){
     console.log(err.toString());
     this.emit("end");

5187263f2e82a2cd3e3c0c9c79aabcc1689771f7
Vignesh Ashokan <vignesh129@gmail.com>
2017-02-08 21:45:06 -0600

    error handling in gulp

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -2,11 +2,15 @@ var gulp = require("gulp"),
     postcss = require("gulp-postcss"),
     autoprefixer = require("autoprefixer"),
     cssvars = require("postcss-simple-vars"),
-    nested = require("postcss-nested")
+    nested = require("postcss-nested"),
     cssImport = require("postcss-import");
 
 gulp.task("styles", function(){
-  gulp.src("./app/assets/styles/styles.css")
+  return gulp.src("./app/assets/styles/styles.css")
   .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+  .on("error", function(err){
+    console.log(err.toString());
+    this.emit("end");
+  })
   .pipe(gulp.dest("./app/temp/styles"));
 });

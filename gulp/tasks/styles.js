b119d676545556f8cfab64e67510082d238fdfef
Vignesh Ashokan <vignesh129@gmail.com>
2017-02-15 18:23:54 -0600

    completed styling of the entire site

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,12 +3,13 @@ var gulp = require("gulp"),
     autoprefixer = require("autoprefixer"),
     cssvars = require("postcss-simple-vars"),
     nested = require("postcss-nested"),
-    cssImport = require("postcss-import");
-    mixins = require("postcss-mixins");
+    cssImport = require("postcss-import"),
+    mixins = require("postcss-mixins"),
+    hexrgba = require("postcss-hexrgba");
 
 gulp.task("styles", function(){
   return gulp.src("./app/assets/styles/styles.css")
-  .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+  .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
   .on("error", function(err){
     console.log(err.toString());
     this.emit("end");
